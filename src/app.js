cb7839aaf9f47a12ab3044483088e10bca60bafe
ajbrodrick <ajbrodrick@gmail.com>
2021-10-31 13:00:09 +1100

    Added search function

--- src/app.js
+++ src/app.js
@@ -22,7 +22,6 @@ function formatDate(timestamp) {
 }
 
 function displayTemperature(response) {
-  console.log(response.data.main.temp);
   let temperature = document.querySelector("#weather-temp");
   let city = document.querySelector("#city");
   let weatherDescription = document.querySelector("#weather-desc");
@@ -30,17 +29,34 @@ function displayTemperature(response) {
   let wind = document.querySelector("#wind");
   let dateNameTime = document.querySelector("#date-dayNameTime");
   let dateDay = document.querySelector("#date-day");
+  let iconElement = document.querySelector("#icon");
   temperature.innerHTML = Math.round(response.data.main.temp);
   city.innerHTML = response.data.name;
   weatherDescription.innerHTML = response.data.weather[0].description;
   humidity.innerHTML = `${response.data.main.humidity} %`;
   wind.innerHTML = `${Math.round(response.data.wind.speed)} km / h`;
   dateNameTime.innerHTML = formatDate(response.data.dt * 1000);
+  iconElement.setAttribute(
+    "src",
+    `http://openweathermap.org/img/wn/${response.data.weather[0].icon}.png`
+  );
+  iconElement.setAttribute("alt", response.data.weather[0].description);
 }
 
-let apiKey = "93d43dfe3b4a950e5b187e5dc313705e";
-let units = "metric";
-let city = "Sydney";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=${units}&appid=${apiKey}`;
+function search(city) {
+  let apiKey = "93d43dfe3b4a950e5b187e5dc313705e";
+  let units = "metric";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=${units}&appid=${apiKey}`;
+  axios.get(apiUrl).then(displayTemperature);
+}
 
-axios.get(apiUrl).then(displayTemperature);
+function changeCity(event) {
+  event.preventDefault();
+  let changeCityElement = document.querySelector("#city-search");
+  search(changeCityElement.value);
+}
+
+search("Sydney");
+
+let form = document.querySelector("#select-city");
+form.addEventListener("submit", changeCity);
